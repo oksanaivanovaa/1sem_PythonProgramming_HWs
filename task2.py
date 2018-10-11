c7645cccf4344e238b00a490d575498b2e209751
Oksana Ivanova <43274752+oksanaivanovaa@users.noreply.github.com>
2018-10-12 00:18:37 +0300

    1st commit 2 task

---
+++ task2.py
@@ -0,0 +1,108 @@
+def listToString(a):
+    s = "["
+    for el in a:
+        s += str(el)+", "
+    if len(a) != 0:
+        s = s[:-2]
+    return s + "]"
+
+
+def addBorder(a):
+    l = len(a[0])
+    for i in range(len(a)):
+        a[i] = "|" + a[i] + "|"
+    b = "+"
+    for i in range(l):
+        b += "-"
+    b += "+"
+    return [b] + a + [b]
+
+
+
+
+def shorting(e):
+    s = ''
+    e_new = []
+    for word in e:
+        if len(word) > 10:
+            word = word[0] + str(len(word[1:-1])) + word[-1]
+        e_new.append(word)
+    return e_new
+
+
+def competition(e, k):
+    if e[k] != 0:
+        res = k+1
+        for i in range(k+1,len(e)-1):
+            if e[i] == e[k]:
+                res += 1
+            else:
+                break
+    else:
+        res = 0
+        for el in e:
+            if el != 0:
+                res += 1
+            break
+    return res
+
+
+def goodPairs(a, b):
+    s = []
+    for i in a:
+        for j in b:
+            if (i*j) % (i+j) == 0:
+                s.append(i**2 + j**2)
+    return sorted(s)
+
+
+def makeShell(n):
+    res = []
+    for i in range(n):
+        res += [[0] * (i + 1)]
+    for i in range(n - 1, 0, -1):
+        res += [[0] * i]
+    return res
+
+
+if __name__ == "__main__":
+
+    assert listToString([]) == "[]", "listToString error"
+    assert listToString([1, 2, 3]) == "[1, 2, 3]", "listToString error"
+    assert listToString([-5]) == "[-5]", "listToString error"
+    print("listToString - OK")
+
+    assert addBorder(['abc',
+                      'def']) == ['+---+',
+                                  '|abc|',
+                                  '|def|',
+                                  '+---+'], \
+        "addBorder error"
+    print("addBorder - OK")
+
+    assert shorting(['word', 'localization', 'internationalization',
+                     'pneumonoultramicroscopicsilicovolcanoconiosis']) == \
+           ['word', 'l10n', 'i18n', 'p43s'], "shorting error"
+    print("shorting - OK")
+
+    assert competition([5, 4, 3, 2, 1], 2) == 3, \
+        "competition error"
+    assert competition([1, 0, 0, 0], 3) == 1, \
+        "competition error"
+    assert competition([10, 9, 8, 7, 7, 7, 5, 5], 4) == 6, \
+        "competition error"
+    print("competition - OK")
+
+    assert goodPairs([4, 5, 6, 7, 8], [8, 9, 10, 11, 12]) == [128, 160, 180], \
+        "goodPairs error"
+    assert goodPairs([2], [2]) == [8], \
+        "goodPairs error"
+    assert goodPairs([7, 8, 9], [5, 3, 2]) == [], \
+        "goodPairs error"
+    print("goodPairs - OK")
+
+    assert makeShell(1) == [[0]], "makeShell error"
+    assert makeShell(2) == [[0], [0, 0], [0]], "makeShell error"
+    assert makeShell(3) == [[0], [0, 0], [0, 0, 0], [0, 0], [0]], \
+        "makeShell error"
+    print("makeShell - OK")

582b797b63c4be46e45bb9d340e5af6e4b6273b9
Oksana Ivanova <43274752+oksanaivanovaa@users.noreply.github.com>
2018-10-24 16:11:32 +0300

    s

--- task4.py
+++ task4.py
@@ -19,27 +19,26 @@ def recurrent(n):
         return 1
     else:
         if n % 2 == 0:
-            return recurrent(n/2)
+            return recurrent(n//2)
         else:
-            return recurrent((n-1)/2)+recurrent((n+1)/2)
+            return recurrent((n-1)//2)+recurrent((n+1)//2)
 
 
-def digitsum(n, res=0):
+def digitsum(n):
     if n//10 == 0:
         if n == 0:
             return 0
         else:
-            return res+n
+            return n
     else:
-        return digitsum(n//10, res+(n % 10))
+        return digitsum(n//10) + n % 10
 
 
-def reversestring(s, i=0):
-    if i >= len(s)-1:
+def reversestring(s):
+    if len(s) <= 1:
         return s
     else:
-        s = s[:i] + s[-1] + s[i:-1]
-        return reversestring(s, i+1)
+        return s[-1] + reversestring(s[0:-1])
 
 
 def ackermann(m, n):
@@ -49,6 +48,23 @@ def ackermann(m, n):
         return ackermann(m-1, 1)
     else:
         return ackermann(m-1, ackermann(m, n-1))
+
+
+def drawborders(n):
+    if n == 1:
+        return ['+']
+    elif n == 2:
+        return ['++',
+                '++']
+    else:
+        middle = drawborders(n-2)
+        for i in range(len(middle)):
+            middle[i] = '|' + middle[i] + '|'
+        plusminus = '+'
+        for i in range(n-2):
+            plusminus += '-'
+        plusminus += '+'
+        return [plusminus] + middle + [plusminus]
 
 
 def genbinarystrings(n):
@@ -109,6 +125,38 @@ def gcd(a, b):
         return gcd(b, a % b)
 
 
+def mergesort(a):
+
+    def merge(left, right):
+
+        res = []
+        i = 0
+        j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        while i < len(left):
+            res.append(left[i])
+            i += 1
+        while j < len(right):
+            res.append(right[j])
+            j += 1
+
+        return res
+
+    if len(a) <= 1:
+        return a
+    else:
+        left = a[:(len(a)//2)]
+        right = a[(len(a)//2):]
+
+        return merge(mergesort(left), mergesort(right))
+
+
 if __name__ == "__main__":
     assert factorial(0) == 1
     assert factorial(2) == 2
@@ -143,6 +191,27 @@ if __name__ == "__main__":
     assert ackermann(2, 5) == 13
     assert ackermann(3, 3) == 61
     print("ackermann - OK")
+
+    assert drawborders(1) == ['+']
+
+    assert drawborders(2) == ['++',
+                              '++']
+
+    assert drawborders(3) == ['+-+',
+                              '|+|',
+                              '+-+']
+
+    assert drawborders(4) == ['+--+',
+                              '|++|',
+                              '|++|',
+                              '+--+']
+
+    assert drawborders(5) == ['+---+',
+                              '|+-+|',
+                              '||+||',
+                              '|+-+|',
+                              '+---+']
+    print('drawborders - OK')
 
     assert genbinarystrings(0) == ['']
     assert genbinarystrings(1) == ['0', '1']
@@ -182,3 +251,9 @@ if __name__ == "__main__":
     assert gcd(18, 12) == 6
     assert gcd(283918822, 595730520) == 22
     print("gcd - OK")
+
+    assert mergesort([]) == []
+    assert mergesort([100]) == [100]
+    assert mergesort([1, 3, 2]) == [1, 2, 3]
+    assert mergesort([1, 3, 5, 4, 2]) == [1, 2, 3, 4, 5]
+    print("mergesort - OK")
